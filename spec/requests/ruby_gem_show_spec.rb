45a93e22d4a4207ef5166e7aedaf92e5bdfe7b1f
Ben Hamill <git-commits@benhamill.com>
2012-01-15 11:30:26 -0600

    Failing spec looking for a form.

--- spec/requests/ruby_gem_show_spec.rb
+++ spec/requests/ruby_gem_show_spec.rb
@@ -12,6 +12,10 @@ describe "Visiting a gem's page" do
   end
 
   it "lists the known versions" do
+    find('.versions').should have_content('0.0.1')
+    find('.versions').should have_content('0.0.2')
+    find('.versions').should have_content('0.1.0')
+    find('.versions').should have_content('0.1.1')
     find('.versions').should have_content('1.0.0')
     find('.versions').should have_content('1.0.1')
   end
@@ -22,5 +26,10 @@ describe "Visiting a gem's page" do
 
   it "displays the changes for the most recent major version number" do
     find('.current_version .changes').should have_content("Fixed bug involving calling NoneSuch::Portal.new when already within another portal's execution block.")
+  end
+
+  it "has a form for comparing two versions" do
+    find('.compare_form').should have_select('.start_version', options: %w(0.0.1 0.0.2 0.1.0 0.1.1 1.0.0))
+    find('.compare_form').should have_select('.end_version', options: %w(0.0.2 0.1.0 0.1.1 1.0.0 1.0.1))
   end
 end
