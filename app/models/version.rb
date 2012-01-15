f97915f89e36f09d32476658bbb68686dc7b832c
Ben Hamill <git-commits@benhamill.com>
2012-01-15 13:10:54 -0600

    Try my best to break up the version number sensibly.

--- app/models/version.rb
+++ app/models/version.rb
@@ -1,4 +1,21 @@
 class Version < ActiveRecord::Base
   belongs_to :ruby_gem
   has_many :changes
+
+  before_save :split_out_version_number
+
+  private
+
+  def split_out_version_number
+    gv = Gem::Version.new(number)
+
+    self.major, self.minor, self.patch = gv.release.segments
+
+    self.prerelease = number.match(/[a-zA-Z]+.*/).to_s
+
+    self.prerelease = nil if self.prerelease.blank?
+    self.major ||= 0
+    self.minor ||= 0
+    self.patch ||= 0
+  end
 end
