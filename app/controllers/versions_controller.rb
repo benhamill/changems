06ee2520e83954d3354745068b6bc5903d761668
Ben Hamill <git-commits@benhamill.com>
2012-01-16 00:10:39 -0600

    Display all the changes in the range.

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -5,5 +5,10 @@ class VersionsController < ApplicationController
   end
 
   def range
+    @start_version = Version.find(params[:start_id])
+    @end_version = Version.find(params[:end_id])
+    @gem = @start_version.ruby_gem
+
+    @versions = VersionDecorator.decorate(@gem.versions.between(@start_version, @end_version))
   end
 end
--- spec/requests/version_range_spec.rb
+++ spec/requests/version_range_spec.rb
@@ -21,12 +21,21 @@ describe "Visiting a version range page" do
   end
 
   it "lists all the changes for each of the versions in the range" do
-    version_lis = all('.changes ul>li')
+    version_lis = all('ul.versions>li')
 
     version_lis[0].should have_content('0.1.1')
-    changes_lis = version_lis[0].all('li')
+    changes_lis = version_lis[0].all('ul.changes>li')
     changes_lis[0].should have_content('Update contributers in gemspec.')
     changes_lis[1].should have_content('Fixed a bug where a NoneSuch::Portal would forget where it was anchored and destroy permanently anything that passed through.')
     changes_lis[2].should have_content('Fixed a divide-by-zero bug.')
+
+    version_lis[1].should have_content('1.0.0')
+    changes_lis = version_lis[1].all('ul.changes>li')
+    changes_lis[0].should have_content('Solidified API.')
+    changes_lis[1].should have_content('Reduced entropy caused by calling NoneSuch::Collider.collide!')
+
+    version_lis[2].should have_content('1.0.1')
+    changes_lis = version_lis[2].all('ul.changes>li')
+    changes_lis[0].should have_content("Fixed bug involving calling NoneSuch::Portal.new when already within another portal's execution block.")
   end
 end
