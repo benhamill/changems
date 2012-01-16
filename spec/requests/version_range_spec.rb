cb2f4482611fea891ef4504d5bfd7ff2552b869e
Ben Hamill <git-commits@benhamill.com>
2012-01-16 00:36:56 -0600

    Failing test to reveal ordering bug.

--- spec/requests/version_range_spec.rb
+++ spec/requests/version_range_spec.rb
@@ -6,7 +6,7 @@ describe "Visiting a version range page" do
   before(:each) do
     visit ruby_gem_url(gem)
 
-    select '0.1.1', from: 'Start version'
+    select '0.0.2', from: 'Start version'
     select '1.0.1', from: 'End version'
 
     click_button 'View Range'
@@ -17,25 +17,39 @@ describe "Visiting a version range page" do
   end
 
   it "lists the version range" do
-    page.should have_content('0.1.1 - 1.0.1')
+    page.should have_content('0.0.2 - 1.0.1')
   end
 
   it "lists all the changes for each of the versions in the range" do
-    version_lis = all('ul.versions>li')
+    version_lis = all('ul.versions>li').to_enum
 
-    version_lis[0].should have_content('0.1.1')
-    changes_lis = version_lis[0].all('ul.changes>li')
-    changes_lis[0].should have_content('Update contributers in gemspec.')
-    changes_lis[1].should have_content('Fixed a bug where a NoneSuch::Portal would forget where it was anchored and destroy permanently anything that passed through.')
-    changes_lis[2].should have_content('Fixed a divide-by-zero bug.')
+    version_li = version_lis.next
+    version_li.should have_content('0.0.2')
+    change_lis = version_li.all('ul.changes>li').to_enum
+    change_lis.next.should have_content("Fix bug in NoneSuch::Portal which increased a programmer's chance of contracting rabies.")
+    change_lis.next.should have_content('Updated README.')
 
-    version_lis[1].should have_content('1.0.0')
-    changes_lis = version_lis[1].all('ul.changes>li')
-    changes_lis[0].should have_content('Solidified API.')
-    changes_lis[1].should have_content('Reduced entropy caused by calling NoneSuch::Collider.collide!')
+    version_li = version_lis.next
+    version_li.should have_content('0.1.0')
+    change_lis = version_li.all('ul.changes>li').to_enum
+    change_lis.next.should have_content('Added direct access to NoneSuch::Collider.')
 
-    version_lis[2].should have_content('1.0.1')
-    changes_lis = version_lis[2].all('ul.changes>li')
-    changes_lis[0].should have_content("Fixed bug involving calling NoneSuch::Portal.new when already within another portal's execution block.")
+    version_li = version_lis.next
+    version_li.should have_content('0.1.1')
+    changes_lis = version_li.all('ul.changes>li').to_enum
+    changes_lis.next.should have_content('Update contributers in gemspec.')
+    changes_lis.next.should have_content('Fixed a bug where a NoneSuch::Portal would forget where it was anchored and destroy permanently anything that passed through.')
+    changes_lis.next.should have_content('Fixed a divide-by-zero bug.')
+
+    version_li = version_lis.next
+    version_li.should have_content('1.0.0')
+    changes_lis = version_li.all('ul.changes>li').to_enum
+    changes_lis.next.should have_content('Solidified API.')
+    changes_lis.next.should have_content('Reduced entropy caused by calling NoneSuch::Collider.collide!')
+
+    version_li = version_lis.next
+    version_li.should have_content('1.0.1')
+    changes_lis = version_li.all('ul.changes>li').to_enum
+    changes_lis.next.should have_content("Fixed bug involving calling NoneSuch::Portal.new when already within another portal's execution block.")
   end
 end
