cb3f06ee925393cc782fa486e6258598f391152e
Ben Hamill <git-commits@benhamill.com>
2012-01-15 23:38:58 -0600

    Elaborate SQL to find ranges of versions in the DB.

--- app/models/version.rb
+++ app/models/version.rb
@@ -13,6 +13,50 @@ class Version < ActiveRecord::Base
     )
   end
 
+  def self.between(start_version, end_version)
+    version_hash = {
+      start_major: start_version.major,
+      start_minor: start_version.minor,
+      start_patch: start_version.patch,
+      start_prerelease: start_version.prerelease,
+      end_major: end_version.major,
+      end_minor: end_version.minor,
+      end_patch: end_version.patch,
+      end_prerelease: end_version.prerelease,
+    }
+
+    where(<<-SQL, version_hash)
+      (
+        versions.major = :start_major AND
+        versions.minor = :start_minor AND
+        versions.patch = :start_patch AND
+        versions.prerelease >= :start_prerelease
+      ) OR (
+        versions.major = :start_major AND
+        versions.minor = :start_minor AND
+        versions.patch > :start_patch
+      ) OR (
+        versions.major = :start_major AND
+        versions.minor > :start_minor
+      ) OR (
+        versions.major > :start_major AND
+        versions.major < :end_major
+      ) OR (
+        versions.major = :end_major AND
+        versions.minor < :end_minor
+      ) OR (
+        versions.major = :end_major AND
+        versions.minor = :end_minor AND
+        versions.patch < :end_patch
+      ) OR (
+        versions.major = :end_major AND
+        versions.minor = :end_minor AND
+        versions.patch = :end_patch AND
+        versions.prerelease <= :end_prerelease
+      )
+    SQL
+  end
+
   private
 
   def split_out_version_number
@@ -21,8 +65,6 @@ class Version < ActiveRecord::Base
     self.major, self.minor, self.patch = gv.release.segments
 
     self.prerelease = number.match(/[a-zA-Z]+.*/).to_s
-
-    self.prerelease = nil if self.prerelease.blank?
     self.major ||= 0
     self.minor ||= 0
     self.patch ||= 0
