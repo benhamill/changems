eba5832fc176c7005e9ad671cb21020e0583714d
Ben Hamill <git-commits@benhamill.com>
2012-01-15 22:42:27 -0600

    All of this is about ranges.

--- spec/requests/version_range_spec.rb
+++ spec/requests/version_range_spec.rb
@@ -11,4 +11,25 @@ describe "Visiting a version range page" do
 
     click_button 'View Range'
   end
+
+  it "shows the gem's name" do
+    pending
+    page.should have_content('none_such')
+  end
+
+  it "lists the version range" do
+    pending
+    page.should have_content('0.1.1 - 1.0.1')
+  end
+
+  it "lists all the changes for each of the versions in the range" do
+    pending
+    version_lis = all('.changes ul>li')
+
+    version_lis[0].should have_content('0.1.1')
+    changes_lis = version_lis[0].all('li')
+    changes_lis[0].should have_content('Update contributers in gemspec.')
+    changes_lis[1].should have_content('Fixed a bug where a NoneSuch::Portal would forget where it was anchored and destroy permanently anything that passed through.')
+    changes_lis[2].should have_content('Fixed a divide-by-zero bug.')
+  end
 end
--- spec/requests/version_show_spec.rb
+++ spec/requests/version_show_spec.rb
@@ -1,23 +1,9 @@
 require 'spec_helper'
 
 describe "Visiting a version's page" do
-  let(:gem) { RubyGem.find_by_name('none_such') }
+  let(:version) { RubyGem.find_by_name('none_such').current_version }
 
-  it "shows the gem's name" do
-    page.should have_content('none_such')
-  end
-
-  it "lists the version range" do
-    page.should have_content('0.1.1 - 1.0.1')
-  end
-
-  it "lists all the changes for each of the versions in the range" do
-    version_lis = all('.changes ul>li')
-
-    version_lis[0].should have_content('0.1.1')
-    changes_lis = version_lis[0].all('li')
-    changes_lis[0].should have_content('Update contributers in gemspec.')
-    changes_lis[1].should have_content('Fixed a bug where a NoneSuch::Portal would forget where it was anchored and destroy permanently anything that passed through.')
-    changes_lis[2].should have_content('Fixed a divide-by-zero bug.')
+  before(:each) do
+    visit version_url(version)
   end
 end
