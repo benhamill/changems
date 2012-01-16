e0252698386e4897d81854f2e0eb8c4b3b434288
Ben Hamill <git-commits@benhamill.com>
2012-01-15 23:40:05 -0600

    Start working on the range specs.

--- spec/requests/version_range_spec.rb
+++ spec/requests/version_range_spec.rb
@@ -4,26 +4,23 @@ describe "Visiting a version range page" do
   let(:gem) { RubyGem.find_by_name('none_such') }
 
   before(:each) do
-    # visit ruby_gem_url(gem)
+    visit ruby_gem_url(gem)
 
-    # select '0.1.1', from: :start_version
-    # select '1.0.1', from: :end_version
+    select '0.1.1', from: :start_version
+    select '1.0.1', from: :end_version
 
-    # click_button 'View Range'
+    click_button 'View Range'
   end
 
   it "shows the gem's name" do
-    pending
     page.should have_content('none_such')
   end
 
   it "lists the version range" do
-    pending
     page.should have_content('0.1.1 - 1.0.1')
   end
 
   it "lists all the changes for each of the versions in the range" do
-    pending
     version_lis = all('.changes ul>li')
 
     version_lis[0].should have_content('0.1.1')
