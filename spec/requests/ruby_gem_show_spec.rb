1c57835e5222ad5b2c2a13c94beb5ac7e7232dad
Ben Hamill <git-commits@benhamill.com>
2012-01-17 20:19:44 -0600

    Failing spec about showing all a gem's changes on one page.

--- spec/requests/ruby_gem_show_spec.rb
+++ spec/requests/ruby_gem_show_spec.rb
@@ -26,8 +26,8 @@ describe "Visiting a gem's page" do
     find('.current_version').should have_content('1.0.1')
   end
 
-  it "displays the all changes for the most recent major version number" do
-    current_version_lis = all('ul.major_version>li').to_enum
+  it "displays the all changes for the gem, most recent first" do
+    current_version_lis = all('ul.versions>li').to_enum
 
     version_li = current_version_lis.next
     version_li.should have_content('1.0.1')
@@ -39,5 +39,28 @@ describe "Visiting a gem's page" do
     changes_lis = version_li.all('ul.changes>li').to_enum
     changes_lis.next.should have_content('Solidified API.')
     changes_lis.next.should have_content('Reduced entropy caused by calling NoneSuch::Collider.collide!')
+
+    version_li = current_version_lis.next
+    version_li.should have_content('0.1.1')
+    changes_lis = version_li.all('ul.changes>li').to_enum
+    changes_lis.next.should have_content('Update contributers in gemspec.')
+    changes_lis.next.should have_content('Fixed a bug where a NoneSuch::Portal would forget where it was anchored and destroy permanently anything that passed through.')
+    changes_lis.next.should have_content('Fixed a divide-by-zero bug.')
+
+    version_li = current_version_lis.next
+    version_li.should have_content('0.1.0')
+    changes_lis = version_li.all('ul.changes>li').to_enum
+    changes_lis.next.should have_content('Added direct access to NoneSuch::Collider.')
+
+    version_li = current_version_lis.next
+    version_li.should have_content('0.0.2')
+    changes_lis = version_li.all('ul.changes>li').to_enum
+    changes_lis.next.should have_content("Fix bug in NoneSuch::Portal which increased a programmer's chance of contracting rabies.")
+    changes_lis.next.should have_content('Updated README.')
+
+    version_li = current_version_lis.next
+    version_li.should have_content('0.0.1')
+    changes_lis = version_li.all('ul.changes>li').to_enum
+    changes_lis.next.should have_content('Initial release.')
   end
 end
