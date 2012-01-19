4fe689620e128bfac0180745343dbfea5668045d
Ben Hamill <git-commits@benhamill.com>
2012-01-18 22:42:50 -0600

    Ordering doesn't matter on these.

--- spec/requests/ruby_gem_show_spec.rb
+++ spec/requests/ruby_gem_show_spec.rb
@@ -31,36 +31,30 @@ describe "Visiting a gem's page" do
 
     version_li = current_version_lis.next
     version_li.should have_content('1.0.1')
-    changes_lis = version_li.all('ul.changes>li').to_enum
-    changes_lis.next.should have_content("Fixed bug involving calling NoneSuch::Portal.new when already within another portal's execution block.")
+    version_li.find('.changes').should have_content("Fixed bug involving calling NoneSuch::Portal.new when already within another portal's execution block.")
 
     version_li = current_version_lis.next
     version_li.should have_content('1.0.0')
-    changes_lis = version_li.all('ul.changes>li').to_enum
-    changes_lis.next.should have_content('Solidified API.')
-    changes_lis.next.should have_content('Reduced entropy caused by calling NoneSuch::Collider.collide!')
+    version_li.find('.changes').should have_content('Solidified API.')
+    version_li.find('.changes').should have_content('Reduced entropy caused by calling NoneSuch::Collider.collide!')
 
     version_li = current_version_lis.next
     version_li.should have_content('0.1.1')
-    changes_lis = version_li.all('ul.changes>li').to_enum
-    changes_lis.next.should have_content('Update contributers in gemspec.')
-    changes_lis.next.should have_content('Fixed a bug where a NoneSuch::Portal would forget where it was anchored and destroy permanently anything that passed through.')
-    changes_lis.next.should have_content('Fixed a divide-by-zero bug.')
+    version_li.find('.changes').should have_content('Update contributers in gemspec.')
+    version_li.find('.changes').should have_content('Fixed a bug where a NoneSuch::Portal would forget where it was anchored and destroy permanently anything that passed through.')
+    version_li.find('.changes').should have_content('Fixed a divide-by-zero bug.')
 
     version_li = current_version_lis.next
     version_li.should have_content('0.1.0')
-    changes_lis = version_li.all('ul.changes>li').to_enum
-    changes_lis.next.should have_content('Added direct access to NoneSuch::Collider.')
+    version_li.find('.changes').should have_content('Added direct access to NoneSuch::Collider.')
 
     version_li = current_version_lis.next
     version_li.should have_content('0.0.2')
-    changes_lis = version_li.all('ul.changes>li').to_enum
-    changes_lis.next.should have_content("Fix bug in NoneSuch::Portal which increased a programmer's chance of contracting rabies.")
-    changes_lis.next.should have_content('Updated README.')
+    version_li.find('.changes').should have_content("Fix bug in NoneSuch::Portal which increased a programmer's chance of contracting rabies.")
+    version_li.find('.changes').should have_content('Updated README.')
 
     version_li = current_version_lis.next
     version_li.should have_content('0.0.1')
-    changes_lis = version_li.all('ul.changes>li').to_enum
-    changes_lis.next.should have_content('Initial release.')
+    version_li.find('.changes').should have_content('Initial release.')
   end
 end
