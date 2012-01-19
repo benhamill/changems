cc35b14d216e84e3207107b6489f0d77f5262e80
Ben Hamill <git-commits@benhamill.com>
2012-01-19 17:22:30 -0600

    update seeds file to have plaintext file extentions.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,29 @@
 RubyGem.create(name: 'none_such') do |gem|
-  gem.versions.build(number: '0.0.1', release_notes: <<-CHANGES)
+  gem.versions.build(number: '0.0.1', release_notes: <<-CHANGES, file_extention: 'txt')
 * Initial release.
 CHANGES
 
-  gem.versions.build(number: '0.1.0', release_notes: <<-CHANGES)
+  gem.versions.build(number: '0.1.0', release_notes: <<-CHANGES, file_extention: 'txt')
 * Added direct access to NoneSuch::Collider.
 CHANGES
 
-  gem.versions.build(number: '0.1.1', release_notes: <<-CHANGES)
+  gem.versions.build(number: '0.1.1', release_notes: <<-CHANGES, file_extention: 'txt')
 * Update contributers in gemspec.
 * Fixed a bug where a NoneSuch::Portal would forget where it was anchored and destroy permanently anything that passed through.
 * Fixed a divide-by-zero bug.
 CHANGES
 
-  gem.versions.build(number: '0.0.2', release_notes: <<-CHANGES)
+  gem.versions.build(number: '0.0.2', release_notes: <<-CHANGES, file_extention: 'txt')
 * Fix bug in NoneSuch::Portal which increased a programmer's chance of contracting rabies.
 * Updated README.
 CHANGES
 
-  gem.versions.build(number: '1.0.0', release_notes: <<-CHANGES)
+  gem.versions.build(number: '1.0.0', release_notes: <<-CHANGES, file_extention: 'txt')
 * Solidified API.
 * Reduced entropy caused by calling NoneSuch::Collider.collide!
 CHANGES
 
-  gem.versions.build(number: '1.0.1', release_notes: <<-CHANGES)
+  gem.versions.build(number: '1.0.1', release_notes: <<-CHANGES, file_extention: 'txt')
 * Fixed bug involving calling NoneSuch::Portal.new when already within another portal's execution block.
 CHANGES
 end
