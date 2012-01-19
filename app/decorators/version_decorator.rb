8e52bd5d50ebbf040146c0d99830078a8985dd16
Ben Hamill <git-commits@benhamill.com>
2012-01-18 23:05:03 -0600

    Well, that was somewhat simpler than I thought.

--- app/decorators/version_decorator.rb
+++ app/decorators/version_decorator.rb
@@ -1,10 +1,6 @@
 class VersionDecorator < ApplicationDecorator
   decorates :version
 
-  def changes
-    version.changes.collect(&:description)
-  end
-
   def link_string
     number.gsub('.', '_')
   end
