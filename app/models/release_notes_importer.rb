cf9b1d0da3565d8daa013217d43daa10b92c72f2
Ben Hamill <git-commits@benhamill.com>
2012-01-22 00:34:58 -0600

    No need for a one-line method here.

--- app/models/release_notes_importer.rb
+++ app/models/release_notes_importer.rb
@@ -4,16 +4,12 @@ module ReleaseNotesImporter
 
     file_extension = FileExtension.from_file_name(file_name)
 
-    versions = pull_apart_release_notes(release_notes, file_extension)
+    versions = ReleaseNotesParser.parse(release_notes, file_extension)
 
     create_needed_versions(versions, file_extension.to_s, gem_name)
   end
 
   private
-
-  def self.pull_apart_release_notes(release_notes, file_extension)
-    # TODO: Parse the file and return a has like { '0.0.1' => "..." }
-  end
 
   def self.create_needed_versions(versions, file_extension, gem_name)
     gem = RubyGem.where(name: gem_name).first
