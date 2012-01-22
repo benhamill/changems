142c72b8bce14493714039b35fc2e5098d50214d
Ben Hamill <git-commits@benhamill.com>
2012-01-21 23:26:56 -0600

    Handle file extension detection slightly differently.

--- app/models/release_notes_importer.rb
+++ app/models/release_notes_importer.rb
@@ -2,14 +2,16 @@ module ReleaseNotesImporter
   def self.import(gem_name)
     release_notes, file_name = ReleaseNotesFetcher.fetch(gem_name)
 
-    versions = pull_apart_release_notes(release_notes)
+    file_extension = rip_extension_off(file_name)
 
-    create_needed_versions(versions, file_name, gem_name)
+    versions = pull_apart_release_notes(release_notes, file_extension)
+
+    create_needed_versions(versions, file_extension, gem_name)
   end
 
   private
 
-  def self.pull_apart_release_notes(release_notes)
+  def self.pull_apart_release_notes(release_notes, file_extension)
     # TODO: Parse the file and return a has like { '0.0.1' => "..." }
   end
 
@@ -20,14 +22,16 @@ module ReleaseNotesImporter
     versions.each do |version_number, release_notes|
       return if Gem::Version.new(version_number) <= latest_known_version
 
-      extension = file_name.rpartition('.').last
-      extension = nil if extension == file_name.partition('.').first
-
       gem.versions.create(
         number: version_number,
         release_notes: release_notes,
-        file_extension: extension
+        file_extension: file_extension
       )
     end
+  end
+
+  def self.rip_extension_off(file_name)
+    return unless file_name =~ /\./
+    extension = file_name.split('.').last
   end
 end
