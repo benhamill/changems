cd1a8f7c892d1892a465ad2ac8ce2ed433ed4516
Ben Hamill <git-commits@benhamill.com>
2012-01-22 01:04:59 -0600

    More complete spec for markdown parser.

--- spec/models/release_notes_parser_spec.rb
+++ spec/models/release_notes_parser_spec.rb
@@ -9,6 +9,19 @@ describe ReleaseNotesParser do
       subject.keys.sort.should == %w(3.1.3 3.1.2 3.1.1 3.1.0).sort
     end
 
-    it "treats text between H2s as release notes"
+    it "treats text between H2s as release notes" do
+      subject['3.1.3'].should == <<-NOTES
+*   Perf fix: If we're deleting all records in an association, don't add a IN(..) clause
+    to the query. *GH 3672*
+
+    *Jon Leighton*
+
+*   Fix bug with referencing other mysql databases in set_table_name. *GH 3690*
+
+*   Fix performance bug with mysql databases on a server with lots of other databses. *GH 3678*
+
+    *Christos Zisopoulos and Kenny J*
+NOTES
+    end
   end
 end
