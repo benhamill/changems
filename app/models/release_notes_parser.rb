714a9bea9f3352a0b89a93e69ab311af1600565f
Ben Hamill <git-commits@benhamill.com>
2012-01-22 00:31:25 -0600

    Skeleton of release notes parser.

---
+++ app/models/release_notes_parser.rb
@@ -0,0 +1,24 @@
+module ReleaseNotesParser
+  class UnknownFileTypeError < StandardError; end
+
+  def self.parse(release_notes, file_extension)
+    parser_for(file_extension).parse(release_notes)
+  end
+
+  private
+
+  def self.parser_for(file_extension)
+    file_extension = FileExtension.new(file_extension)
+
+    if file_extension.markdown?
+      ReleaseNotesParser::Markdown
+    else
+      raise ReleaseNotesParser::UnknownFileTypeError, "Cannot parse files of type #{file_extension.to_s.inspect}"
+    end
+  end
+
+  module Markdown
+    def self.parse(release_notes)
+    end
+  end
+end
