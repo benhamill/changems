7dd91d1fa46d6e5c5a4be1d05e3088bae0417c16
Ben Hamill <git-commits@benhamill.com>
2012-01-23 00:22:00 -0600

    Now you can ask a gem to import its own release notes.

--- app/models/ruby_gem.rb
+++ app/models/ruby_gem.rb
@@ -4,4 +4,8 @@ class RubyGem < ActiveRecord::Base
   def current_version
     versions.semantic_order('desc').limit(1).first
   end
+
+  def import_release_notes
+    ReleaseNotesImporter.import(name)
+  end
 end
