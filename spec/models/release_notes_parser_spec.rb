ff76303507b82168152794a5366bc2e9460f0650
Ben Hamill <git-commits@benhamill.com>
2012-01-28 00:04:39 -0600

    Tighten up (and stricten up) the rdoc specs.

--- spec/models/release_notes_parser_spec.rb
+++ spec/models/release_notes_parser_spec.rb
@@ -33,21 +33,28 @@ NOTES
     subject { ReleaseNotesParser::RDoc.parse(rdoc_release_notes) }
 
     it "pulls version numbers from H2s" do
-      subject.keys.sort.should == %w(2).sort
+      subject.keys.sort.should == %w(1.5.1 1.5.0 1.4.7 1.4.6 1.4.5).sort
     end
 
     it "treats text between H2s as release notes" do
-      subject['3.1.3'].should == <<-NOTES
-*   Perf fix: If we're deleting all records in an association, don't add a IN(..) clause
-    to the query. *GH 3672*
+      subject['1.5.0'].should == <<-NOTES
+* Notes
 
-    *Jon Leighton*
+  * See changelog from 1.4.7
 
-*   Fix bug with referencing other mysql databases in set_table_name. *GH ##3690*
+* Features
 
-*   Fix performance bug with mysql databases on a server with lots of other databses. *GH 3678*
+  * extracted sets of Node::SaveOptions into Node::SaveOptions::DEFAULT_{X,H,XH}TML (refactor)
 
-    *Christos Zisopoulos and Kenny J*
+* Bugfixes
+
+  * default output of XML on JRuby is no longer formatted due to
+    inconsistent whitespace handling. #415
+  * (JRuby) making empty NodeSets with null `nodes` member safe to operate on. #443
+  * Fix a bug in advanced encoding detection that leads to partially
+    duplicated document when parsing an HTML file with unknown
+    encoding.
+  * Add support for <meta charset="...">.
 
 NOTES
     end
