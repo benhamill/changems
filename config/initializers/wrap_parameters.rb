f80c808b02015d9a7fb740035bde302016ae484e
Kir Shatrov <shatrov@me.com>
2013-08-02 15:57:14 +0400

    Better json format

--- config/initializers/wrap_parameters.rb
+++ config/initializers/wrap_parameters.rb
@@ -12,3 +12,11 @@ end
 # ActiveSupport.on_load(:active_record) do
 #  self.include_root_in_json = true
 # end
+
+ActiveSupport.on_load(:active_model_serializers) do
+  # Disable for all serializers (except ArraySerializer)
+  ActiveModel::Serializer.root = false
+
+  # Disable for ArraySerializer
+  ActiveModel::ArraySerializer.root = false
+end
